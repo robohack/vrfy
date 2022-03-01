be0f3b822585396b365ff72a2bfd308addb60513
Greg A. Woods <woods@planix.ca>
2022-02-28 17:19:57 -0800

    ready for v20220228 release

--- vers.c
+++ vers.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char Version[] = "@(#)vers.c	woods@planix.ca (Greg A. Woods) 20191122";
+static char Version[] = "@(#)vers.c	woods@planix.ca (Greg A. Woods) 20220228";
 #endif
 
-char *version = "20191122";
+char *version = "20220228";
